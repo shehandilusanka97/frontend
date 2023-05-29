9a4571a269bd7779367cede527e6f245bc831acd
shehandilusanka97 <dilumzsanox9@gmail.com>
2023-05-30 03:54:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {LoginPage} from './Routes.js';
 
 const App = () => {
   return (
-    <div>App</div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/login' element={<LoginPage/>}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
 
