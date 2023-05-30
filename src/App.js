cb3daa19f4bd869264a697f4192b69f867a6c07e
shehandilusanka97 <dilumzsanox9@gmail.com>
2023-05-30 13:07:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import {LoginPage} from './Routes.js';
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="min-h-screen bg-gray-50 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:max-w-md">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Login to your account
+            </h2>
+        </div>
+        <div className="mt-8 mx-auto sm:w-full sm:max-w-md">
+            <dv className="bg-white py-8 px-4 shadow sm-rounded-lg sm:px-10">
+                <form className="space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                            Email address
+                        </label>
+                    </div>
+                </form>
+            </dv>
 
-export default Login
+        </div>
+    </div>
+  );
+};
+
+export default Login;
